573a4e9981c70ce46c81932e5fe1df4acd0c902a
Nicolana <nicolanazwj@gmail.com>
2021-06-04 21:11:03 +0800

    feat: 新增useMount Hooks

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
 import qs from "qs";
-import { cleanObject } from "../../utils";
+import { cleanObject, useMount } from "../../utils";
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -21,13 +21,13 @@ export const ProjectListScreen = () => {
     })
   }, [param])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
 
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 
 // 在一个函数里，改变传入的对象本身是不好的
@@ -12,4 +14,22 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const debounce = (func, delay) => {
+  let timeout;
+  return (...params) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      func(...params);
+    }, delay);
+  };
 };
