58213a6b2e2e6807d6c7c322ee25763daf3dd322
Nicolana <nicolanazwj@gmail.com>
2021-06-04 09:10:14 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,34 @@
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { useEffect, useState } from "react";
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  })
+  const [users, setUsers] = useState([])
+  const [list, setList] = useState([])
+  useEffect(() => {
+    fetch(`${apiUrl}/projects`).then(async response => {
+      if (response.ok) {
+        setList(await response.json())
+      }
+    })
+  }, [param])
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async response => {
+      if (response.ok) {
+        setUsers(await response.json())
+      }
+    })
+  }, [users])
+
+  return <div>
+    <SearchPanel users={users} param={param} setParam={setParam} />
+    <List users={users} list={list}/>
+  </div>
+}
