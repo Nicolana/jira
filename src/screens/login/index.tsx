44ca0391890efbbe54de95b96daf45b95e04334d
之一 <nicolanazwj@gmail.com>
2021-07-15 08:45:02 +0800

    chore: 安装dev-tools

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,7 +1,28 @@
 import { FormEvent } from "react";
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 export const LoginScreen = () => {
+  const login = (params: { username: string; password: string }) => {
+    fetch(`${apiUrl}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    }).then(async (response: Response) => {
+      if (response.ok) {
+        console.log("OK");
+      }
+    });
+  };
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const username = (event.currentTarget.elements[0] as HTMLInputElement)
+      .value;
+    const password = (event.currentTarget.elements[1] as HTMLInputElement)
+      .value;
+    login({ username, password });
     console.log("handled submit ");
   };
   return (
